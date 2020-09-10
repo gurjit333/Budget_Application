98a937ced9f84fd3608a07a5240babd85ad4ce92
gurjit333 <70355953+gurjit333@users.noreply.github.com>
2020-09-11 09:20:59 +0930

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function(){
       navigator.serviceWorker.register('/sw.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
